781db70fd8dd5e8f700abc3f92ca6fa2b4e5c9e2
Leejungchan1 <wjdcks0123@naver.com>
2024-07-08 17:47:58 +0900

    ujin1

---
+++ js/ujin.js
@@ -0,0 +1,40 @@
+$(function(){
+    $(".main li").mouseover(function(){
+        $(".sub,.sub_bg").stop().slideDown();
+
+
+    }) /* mouseover */
+
+    $(".main li").mouseout(function(){
+        $(".sub,.sub_bg").stop().slideUp();
+
+
+    }) /* mouseout */
+
+    // 이미지 영역
+
+    $(".fade li").eq(0).siblings().hide();
+    // $(".fade li").hide();
+    // $(".fade li").eq(0).show();
+
+    var n = 0; // 0 1 2
+    setInterval(function(){
+        if(n == 2){
+            n=0;
+        }else{
+            n++;
+        } //if
+        $(".fade li").eq(n).fadeIn().siblings().fadeOut();
+
+        console.log(n);
+    },2000)
+
+
+
+
+
+
+
+
+
+}) // jQuery end
